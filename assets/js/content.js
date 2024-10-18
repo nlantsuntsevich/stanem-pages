a377e2493163a5ad9199c80a10ac4377ed316ba7
Natallia Lantsuntsevich <117663787+nlantsuntsevich@users.noreply.github.com>
2024-10-18 16:23:43 +0000

    item

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,7 +1,7 @@
-var $container = $('.mm-columns');
-$container.imagesLoaded(function(){
-  $container.masonry({
-    itemSelector : '.mm-columns__item',
-    percentPosition: true
+var $grid = $('.mm-columns').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.mm-columns__item',
+      percentPosition: true,
+      columnWidth: '.mm-columns__item'
+    }); 
   });
-});
